799a6abc6a37d2aae441e42d88c84d09475d969c
Glumpy <100801667+GlumpGod@users.noreply.github.com>
2023-02-13 00:44:15 -0600

    changed text slideshow and links

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var slideIndex = 0;
-    showSlides();
+showSlides();
 
-    function showSlides() {
-      var i;
-      var slides = document.getElementsByClassName("Slides");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
